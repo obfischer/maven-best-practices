9f1712070300285935f88b395ef089cc916daf76
Oliver B. Fischer <o.b.fischer@swe-blog.net>
2026-03-11 23:15:14 +0100

    Improve the quality of the used shell scripts
    All shell scripts have been linted with
    shellcheck to improve the quality of the
    scripts.

--- run-antora.sh
+++ run-antora.sh
@@ -4,9 +4,9 @@ set -e -u -o pipefail
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-if [ ! $(2>&- command -v npx) ]; then
+if [ ! "$(2>&- command -v npx)" ]; then
   1>&2 echo npx could not be found
   exit 1
 fi
 
-npx antora ${SCRIPT_DIR}/antora-playbook.yml
+npx antora "${SCRIPT_DIR}/antora-playbook.yml"
